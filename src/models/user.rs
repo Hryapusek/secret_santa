12c78a5f23556856a01eee22320fa8f1b39d36e4
Vyacheslav <slava.gusarov.73@mail.ru>
2023-01-08 01:37:52 +0300

    Поднял сервер написал /version

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,13 +1,14 @@
 use super::super::schema::*;
 use diesel::prelude::*;
+use serde::{Serialize, Deserialize};
 
-#[derive(Queryable, Clone, Identifiable)]
+#[derive(Queryable, Clone, Identifiable, Serialize, Deserialize)]
 pub struct User {
     pub id: i32,
     pub name: String,
 }
 
-#[derive(Insertable)]
+#[derive(Insertable, Serialize, Deserialize)]
 #[table_name = "users"]
 pub struct NewUser {
     pub name: String,
